dbd08c31be1b3bd0cf867a516bd6e19050a21c37
serinuntius <wasab1gj@gmail.com>
2018-09-23 04:38:22 +0900

    add post form

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-});
- 
-app.listen(3000);
 
-console.log('access http://localhost:3000');
+// app.use(reload(__dirname + '/app.js'))
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+
+app.get('/', function (req, res) {
+    const response = {
+        "message": '',
+    };
+    res.render("./index.ejs", response);
+});
+
+app.post('/quizzes/create', function (req, res) {
+    const response = {
+        "message": "登録が成功しました。",
+    };
+
+    res.render("./index.ejs", response);
+});
+
+
+ 
+app.listen(3000, () => console.log('access http://localhost:3000'))
